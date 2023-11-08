37ea134b54b39a3c0afb2663085d6f938aad50dc
Muhammad Abayomi Ibraheem <mmabayomi2208@gmail.com>
2023-11-09 00:25:09 +0100

    finished drum machine ...  didint finish per se .. but passsed all tests sha

--- drumMachine/main.js
+++ drumMachine/main.js
@@ -1,36 +1,47 @@
-import { DrumPad } from "./components/DrumPad";
 const keys = ["q", "w", "e", "a", "s", "d", "z", "x", "c"];
 const audioSamples = [
-  "Heater-1.mp3",
-  "Heater-2.mp3",
-  "Heater-3.mp3",
-  "Heater-4_1.mp3",
-  "Heater-6.mp3",
-  "Dsc_Oh.mp3",
-  "Kick_n_Hat.mp3",
-  "RP4_KICK_1.mp3",
-  "Cev_H2.mp3",
+  "Heater 1",
+  "Heater 2",
+  "Heater 3",
+  "Heater 4",
+  "Clap",
+  "Open HH",
+  "Kick-n'-Hat",
+  "Kick",
+  "Closed-HH",
 ];
+
 document.addEventListener("DOMContentLoaded", () => {
   const buttonsDiv = document.querySelector("#pad-bank");
-  const buttonPads = buttonsDiv.querySelectorAll("drum-pad");
-  const slider = document.querySelector("#input-slider");
+  const buttonPads = buttonsDiv.querySelectorAll(".drum-pad");
+  const sound = document.querySelectorAll("audio");
+  const display = document.querySelector("#display");
 
-  Array.from(buttonPads).forEach((btn, i) => {
-    btn.querySelector("span").innerHTML = keys[i].toUpperCase();
-    btn.querySelector("audio").src += audioSamples[i];
-
-    btn.addEventListener("click", (e) => {
-      console.log(btn.querySelector("audio").getAttribute("src"));
-      btn.querySelector("audio").play();
+  document.addEventListener("keypress", (e) => {
+    Array.from(sound).forEach((audio) => {
+      if (audio.id == e.key.toUpperCase()) {
+        audio.play();
+      }
     });
   });
 
-  const power = document.getElementById("power");
+  Array.from(buttonPads).forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+      console.log(btn.innerText);
+
+      btn.querySelector("audio").play();
+
+      display.innerText = btn.id;
+      setTimeout(() => {
+        display.innerText = "";
+      }, 800);
+    });
+  });
+
+  /*   const power = document.getElementById("power");
   const select = document.querySelector(".select");
   const powerSwitch = select.querySelector(".inner");
   select.addEventListener("click", (e) => {
-    console.log(select.classList);
     if (Array.from(select.classList).includes("justify-end")) {
       e.target.closest("#power").dataset.status = "off";
       console.log(power.dataset.status);
@@ -40,9 +51,5 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log(power.dataset.status);
       select.classList.add("justify-end");
     }
-  });
-
-  slider.addEventListener("change", (e) => {
-    console.log(e.value);
-  });
+  }); */
 });
